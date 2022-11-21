1d606ea1b33b4d4047d7fba6b2b5696b7e2fa5e9
Suraj Dhabale <surajdhabale60@gmail.com>
2022-11-21 22:03:38 +0530

    [Suraj]Add:Enqueue Element Into Stack

--- StackAndQueue/StackAndQueue/Program.cs
+++ StackAndQueue/StackAndQueue/Program.cs
@@ -4,16 +4,29 @@
     {
         public static void Main(string[] args)
         {
-            LinkedListStack linkedListStack = new LinkedListStack();
-            linkedListStack.Push(56);
-            linkedListStack.Push(30);
-            linkedListStack.Push(70);
-            linkedListStack.Peek();
-            linkedListStack.Pop();
-            
+            //LinkedListStack linkedListStack = new LinkedListStack();
+            //linkedListStack.Push(56);
+            //linkedListStack.Push(30);
+            //linkedListStack.Push(70);
+
+            //linkedListStack.Peek();
+            //linkedListStack.Pop();
+
+            //linkedListStack.Display();
+
+            LinkedListQueue linkedListQueue = new LinkedListQueue();
+            linkedListQueue.Enqueue(56);
+            linkedListQueue.Enqueue(30);
+            linkedListQueue.Enqueue(70);
+
+            linkedListQueue.Display();
 
 
-            linkedListStack.Display();
+
+
+
+
+
         }
     }
 }
