2f9ee089554fc4ffd8684bf5d0751354c0f9bf67
Suraj Dhabale <surajdhabale60@gmail.com>
2022-11-21 20:40:11 +0530

    [Suraj]Add:Pushing Element To Stack

---
+++ StackAndQueue/StackAndQueue/LinkedListStack.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace StackAndQueue
+{
+    internal class LinkedListStack
+    {
+        private Node top;
+        public LinkedListStack()
+        {
+            this.top = null;
+        }
+        internal void Push(int value)//add method
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine("{0} pushed to stack", value);
+        }
+        internal void Display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.WriteLine("After Pused to stack " + temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
---
+++ StackAndQueue/StackAndQueue/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace StackAndQueue
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            LinkedListStack linkedListStack = new LinkedListStack();
+            linkedListStack.Push(56);
+            linkedListStack.Push(30);
+            linkedListStack.Push(70);
+
+
+            linkedListStack.Display();
+        }
+    }
+}
